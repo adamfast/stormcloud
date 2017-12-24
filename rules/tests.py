198d0898190b0f756ebaf01a8212972a454dffa4
Adam Fast <adam@adamfast.com>
2017-12-24 11:31:08 -0600

    Middleware automatically creates rules for incoming requests, and is capable of serving static responses.

--- rules/tests.py
+++ rules/tests.py
@@ -7,9 +7,16 @@ from rules.models import Rule
 
 
 class StormCloudMiddlewareTests(TestCase):
-    def test_request(self):
+    def test_request_creates_rule_entry(self):
         self.assertEqual(Rule.objects.all().count(), 0)
 
         response = self.client.get('/stripe/post/')
 
         self.assertEqual(Rule.objects.all().count(), 1)
+
+    def test_request_gets_static_response(self):
+        rule = Rule.objects.create(hostname='testserver', path='/stormcloud-test/', verb='GET',
+                                   action='flat', flat_response='unicycle cat')
+
+        response = self.client.get(rule.path)
+        self.assertEqual(response.content, rule.flat_response)
--- stormcloud/middleware.py
+++ stormcloud/middleware.py
@@ -1,14 +1,61 @@
+from django.http import HttpResponse
+
+from rules.models import Rule
+from vendors.models import Vendor
+
 
 class StormCloudMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
 
+    def vendor_lookup(self, server_name):
+        vendors = Vendor.objects.filter(base_url=server_name)
+        if vendors.count() > 1:
+            raise Exception("More than one vendor for that hostname")
+
+        return vendors.first()
+
+    def rule_lookup(self, vendor, path, verb):
+        lookup_kwargs = {}
+        if vendor:
+            lookup_kwargs['vendor__in'] = vendor
+
+        lookup_kwargs['path'] = path
+        lookup_kwargs['verb'] = verb
+
+        rules = Rule.objects.filter(**lookup_kwargs)
+
+        if rules.count() > 1:
+            raise Exception("More than one rule for that path / vendor")
+
+        return rules.first()
+
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        response = self.get_response(request)
+        vendor = self.vendor_lookup(request.META['SERVER_NAME'])
+        rule = self.rule_lookup(vendor=vendor, path=request.META['PATH_INFO'], verb=request.META['REQUEST_METHOD'])
+
+        if not rule:  # nothing found
+            # go ahead and create one with what we know
+            rule = Rule.objects.create(vendor=vendor, hostname=request.META['SERVER_NAME'], path=request.META['PATH_INFO'],
+                                       verb=request.META['REQUEST_METHOD'])
+            return HttpResponse("")  # blank response initially
+
+        # todo: some better reading of GET/POST parameters to make rules more fine-grained, may require architectural changes
+        # str(request.GET.dict())
+        # str(request.POST.dict())
+
+        if not rule.action:  # no action configured for this URL
+            return None  # act normally / return to Django
+
+        if rule.action == 'flat':
+            return HttpResponse(rule.flat_response)
+
+        response = HttpResponse('')  # respond with nothing to everything else
+        # response = self.get_response(request)
 
         # Code to be executed for each request/response after
         # the view is called.
