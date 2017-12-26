b69eafd1df6a9e3fbc3b840aee0a27c4e836308d
Adam Fast <adam@adamfast.com>
2017-12-26 16:13:41 -0600

    Set up admin classes for the models.

--- rules/admin.py
+++ rules/admin.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from rules.models import *
+
+
+class RuleResponseInline(admin.TabularInline):
+    model = RuleResponse
+
+
+class RuleSubstitutionInline(admin.TabularInline):
+    model = RuleSubstitution
+
+
+class RuleAdmin(admin.ModelAdmin):
+    list_display = ('vendor', 'hostname', 'path', 'verb', 'action', 'delay_ms', 'live_url')
+    list_filter = ('vendor', 'verb', 'action')
+    inlines = (RuleResponseInline, RuleSubstitutionInline)
+
+admin.site.register(Rule, RuleAdmin)
--- vendors/admin.py
+++ vendors/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from vendors.models import *
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'base_url')
+
+admin.site.register(Vendor, VendorAdmin)
